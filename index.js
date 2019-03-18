ac8e9752c8065411db84e17fb834ffaa8bf75123
Juliano Penna <julianopenna@gmail.com>
2019-03-18 14:31:40 +0800

    Adjust to Heroku

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 require('dotenv').config();
 const WebSocket = require('ws');
-const Room = require('./src/Room');
+const http = require('http');
 
+const Room = require('./src/Room');
 const { setup } = require('./src/setup');
+
+const port = process.env.PORT || 17000;
+const server = http.createServer((req, res) => {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
+  res.end('Hello, this is just for Websocket :)');
+});
+server.listen(port);
+
+console.info(`HTTP and WS server listening on ${port}`);
 
 const room = new Room();
 
-const wss = new WebSocket.Server({
-  port: process.env.PORT || 17000,
-});
+const wss = new WebSocket.Server({ server });
 
 wss.broadcast = (data) => {
   wss.clients.forEach((client) => {
--- wsTest.js
+++ wsTest.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const WebSocket = require('ws');
 
-const ws = new WebSocket(`ws://localhost:${process.env.WS_PORT || 17000}`);
+const ws = new WebSocket(`ws://localhost:${process.env.PORT || 17000}`);
 
 const sendOriginal = ws.send;
 ws.send = function send(data) {
